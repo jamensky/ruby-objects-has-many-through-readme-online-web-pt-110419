dc682e902fc46df14de841bd9a490a1fb99885fb
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-24 16:44:18 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,11 +6,11 @@ class Customer
    def initialize(name, age)
      @name = name
      @age = age
-     @@all << self 
+     @@all << self
    end
 
-   def self.all 
-     @@all 
-   end 
+   def self.all
+     @@all
+   end
 
 end
